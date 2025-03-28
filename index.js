03cb7f2c31cb5630693e9923319c41963ee178ca
jarde765 <ribeiroamarelo12@gamail.com>
2025-03-28 11:20:03 -0300

    Processamento dos dados da API

---
+++ index.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('.input')
+const button = document.querySelector('.button')
+
+const infosFn = async (nome) => {
+    try {
+        const resposta = await fetch(`https://api.github.com/users/${}`)
+        const data = await resposta.json()
+        console.log(data)
+    } catch (err) {
+          console.log(err)
+    }
+}
+
+const reposFn = async (nome) => {
+    try {
+        const resposta = await fetch(`https://api.github.com/users/${nome}/repos`)
+        const data = await resposta.json()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+button.addEventListener('click', () => {
+    document.querySelector('.infos').innerHTML = input
+    reposFn(input.value)
+})
